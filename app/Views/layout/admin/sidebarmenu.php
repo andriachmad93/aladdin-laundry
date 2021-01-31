d16da24e5bb03723a0a446a172f05daa842f7e68
unknown <andriyansyah@integrasolution.id>
2021-01-31 16:04:47 +0700

    add icon

--- app/Views/layout/admin/sidebarmenu.php
+++ app/Views/layout/admin/sidebarmenu.php
@@ -3,31 +3,27 @@
         <ul class="nav flex-column">
             <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="<?= base_url('admin'); ?>">
-                    <span data-feather="home"></span>
-                    Dashboard
+                    <i class="fa fa-home"></i><span data-feather="home"> Beranda</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="<?= base_url('order'); ?>">
-                    <span data-feather="file"></span>
-                    Pesanan
+                    <i class="fa fa-shopping-cart"></i><span data-feather="file"> Pesanan</span>
                 </a>
             </li>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="<?= base_url('item'); ?>">
-                    <span data-feather="users"></span>
-                    Layanan
+                    <i class="fa fa-tshirt"></i><span data-feather="users"> Layanan</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="<?= base_url('promotion'); ?>">
-                    <span data-feather="bar-chart-2"></span>
-                    Promo
+                    <i class="fa fa-percent"></i><span data-feather="bar-chart-2"> Promo</span>
                 </a>
             </li>
-            <li class="nav-item"><a class="nav-link" href="<?= base_url('customer'); ?>"><span>Pelanggan</span></a></li>
-            <li class="nav-item"><a class="nav-link" href="<?= base_url('user/user_group'); ?>"><span>Hak Akses</span></a></li>
+            <li class="nav-item"><a class="nav-link" href="<?= base_url('customer'); ?>"><i class="fa fa-user"></i><span> Pelanggan</span></a></li>
+            <li class="nav-item"><a class="nav-link" href="<?= base_url('user/user_group'); ?>"><i class="fa fa-users"></i><span> Hak Akses</span></a></li>
             <li class="nav-item">
                 <a class="nav-link collapsed text-truncate" href="#submenu1" data-toggle="collapse" data-target="#submenu1"><i class="fa fa-table"></i> <span class="d-none d-sm-inline">Laporan</span></a>
                 <div class="collapse" id="submenu1" aria-expanded="false">
